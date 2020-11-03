ef6de446c3c4cc38daaed6694df47d2702f5257a
SpaceGarbage <SpaceGarbage@users.noreply.github.com>
2020-11-03 20:53:29 +0100

    Yes

--- JS/pong.js
+++ JS/pong.js
@@ -1,14 +1,14 @@
-//Définir des variables du script
+//Définir les variables du script
 
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
 
-//Script déplacement balle
+//Script déplacement de la balle
 setInterval(function(){
     gauche = gauche + 1;
     haut = haut + 0.5;
-    // Affiche le résultat de I dans le titre de page toutes les 10 millisecondes 
+    //Affiche le résultat de I dans le titre de page toutes les 10 millisecondes 
     $("#balle").css("left",gauche); 
     $("#balle").css("top",haut);
 
